9725cd5dea98d4e96fae0eef4d0e62feb22f6fbe
YENDHI JHOANA KOVAC PARROQUIN <yendhikovacp@nube.unadmexico.mx>
2020-06-25 12:08:30 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CIV.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace CIV.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration configuration;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public HomeController(IConfiguration config)
+        {
+            configuration = config;
+        }
+        public IActionResult InicioSesion()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ValeIngresar()
+        {
+            return View();
+        }
+        public IActionResult ValeEstatus()
+        {
+            return View();
+        }
+        public IActionResult InventarioIngresar()
+        {
+            return View();
+        }
+        public IActionResult InventarioEstatus()
+        {
+            return View();
+        }
+        public IActionResult Reporte()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
